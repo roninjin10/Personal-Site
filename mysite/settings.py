2fbcd181cae5a231b11bd6329ddbd2a3e26bded6
William Cory <willcory10@gmail.com>
2018-02-27 16:33:27 -0500

    added heroku database

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,6 +89,11 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL for heroku deployment
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
