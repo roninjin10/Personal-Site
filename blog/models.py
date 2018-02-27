97414cf1619c64ab93958300a546aa234a15bb48
William Cory <willcory10@gmail.com>
2018-02-27 18:24:30 -0500

    make default value for models

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
   title = models.CharField(max_length=140)
   body = models.TextField()
-  html = models.TextField()
+  html = models.TextField(default='no html here')
   date = models.DateTimeField()
 
   def __str__(self):
