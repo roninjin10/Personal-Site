20256db2194dd8862bd14272e9f40a86e5a67c12
William Cory <willcory10@gmail.com>
2018-02-28 09:26:45 -0500

    change blog to parse paragraphs and remove html from models

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Post(models.Model):
   title = models.CharField(max_length=140)
   body = models.TextField()
-  html = models.TextField(default='no post found')
   date = models.DateTimeField()
 
   def __str__(self):
     return self.title
+
+  def body_as_list(self):
+    return self.body.split('\n')
