d5a28349a59cae4dd3debeed618de5b6832914f5
William Cory <willcory10@gmail.com>
2018-02-27 23:18:19 -0500

    fix blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from blog.models import Post
+from django.http import HttpResponse
 
 def blog_post(request, title):
   try:
-    b = Post.objects.filter(title= title.replace('-',' '))
+    b = Post.objects.filter(title= ' '.join(title.split('-')))[0]
   except Post.DoesNotExist:
     raise Http404("Post does not exist")
   return render(request, 'blog/blog_post.html', {'content': [b]})
