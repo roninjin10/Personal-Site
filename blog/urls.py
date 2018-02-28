9cd3db153af4915af37afc855b0bc5452f0a779e
William Cory <willcory10@gmail.com>
2018-02-27 22:30:17 -0500

    fix syntax error in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', ListView.as_view(queryset=Post.objects.all().order_by('-date'), template_name='blog/blog.html')),
-    path('<str:title>/', views.blog_post)
+    path('<str:title>/', views.blog_post),
+]
