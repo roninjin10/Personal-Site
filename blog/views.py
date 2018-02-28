2075a2d00e38ee798f85a9c09554bd84ba0ba437
William Cory <willcory10@gmail.com>
2018-02-27 20:46:25 -0500

    change dashes to spaces and vice versa for blog titles

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from blog.models import Post
 
 def blog_post(request, title):
-  b = Blog(title=title)
+  b = Blog(title=title.replace('-',' '))
   return render(request, 'blog/blog_post.html', {'content': b})
