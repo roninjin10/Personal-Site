35dac41ec183856f852d7efe22a12750d96ca18d
William Cory <willcory10@gmail.com>
2018-02-27 23:04:49 -0500

    change models.py to use objects.filter

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from blog.models import Post
 
 def blog_post(request, title):
   try:
-    b = Post(title= title.replace('-',' '))
+    b = Post.objects.filter(title= title.replace('-',' '))
   except Post.DoesNotExist:
     raise Http404("Post does not exist")
   return render(request, 'blog/blog_post.html', {'content': [b]})
